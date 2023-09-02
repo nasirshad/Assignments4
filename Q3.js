cba7bb7714c1c9366b3b9dfbe3111b5f9867bb97
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-02 12:38:59 +0500

    Message

---
+++ Q3.js
@@ -0,0 +1,8 @@
+"use strict";
+// Q No.3.	Write a program that uses a while loop 
+// to print the first 25 integers.
+let integer = 18;
+while (integer <= 25) {
+    console.log(integer);
+    integer++;
+}
